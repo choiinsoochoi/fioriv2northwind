670e7a040aacf0e17f447a89a699dcb8a74e7298
insoochoi <cis940320@gmail.com>
2023-09-28 08:33:46 +0000

    init

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["fioriv2northwind/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"fioriv2northwind/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
